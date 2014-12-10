6040ccfdb26b780bcc72d609b0389a3bd72d002f
PrzemekBurczyk <przemek.burczyk@gmail.com>
2014-12-10 22:54:51 +0100

    Implemented payload splitting when it exceeds maximum buffer size of UDP protocol.

--- lib/src/main/java/pl/edu/agh/marims/screenstreamer/lib/network/sender/UdpSocketSender.java
+++ lib/src/main/java/pl/edu/agh/marims/screenstreamer/lib/network/sender/UdpSocketSender.java
@@ -14,6 +14,8 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.edu.agh.marims.screenstreamer.lib.convert.BitmapToBase64Converter;
 import pl.edu.agh.marims.screenstreamer.lib.screen.intercepter.Intercepter;
@@ -23,6 +25,8 @@ import pl.edu.agh.marims.screenstreamer.lib.screen.intercepter.ScreenIntercepter
  * Created by Przemek on 2014-12-10.
  */
 public class UdpSocketSender extends AbstractSender {
+
+    private static final boolean DEBUG = true;
 
     private Handler mainLooper = new Handler(Looper.getMainLooper());
     private BitmapToBase64Converter converter;
@@ -43,6 +47,8 @@ public class UdpSocketSender extends AbstractSender {
 
     private class SenderWorker extends Thread {
 
+        private static final int MAX_SIZE = 65000;
+
         private InetAddress address;
         private DatagramSocket socket;
         private Bitmap bitmap;
@@ -54,38 +60,51 @@ public class UdpSocketSender extends AbstractSender {
         public void run() {
             lastScreenshotVersion = screenshotVersion;
             try {
-                socket = new DatagramSocket(6666);
+                socket = new DatagramSocket(5000);
                 address = InetAddress.getByName("192.168.0.11");
+                while (runSending) {
+                    if (!loadInProgress) {
+                        try {
+                            send();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        load();
+                    }
+                }
+                if (socket != null && !socket.isClosed()) {
+                    socket.close();
+                }
             } catch (SocketException e) {
                 e.printStackTrace();
             } catch (UnknownHostException e) {
                 e.printStackTrace();
             }
-            while (runSending) {
-                if (!loadInProgress) {
-                    try {
-                        send();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    load();
-                }
-            }
         }
 
         private void send() throws IOException {
             if (lastScreenshotVersion != screenshotVersion) {
-                byte[] payload = buildPayload().getBytes();
-                Log.d("REQUEST", "Post length: " + payload.length);
+                List<byte[]> payloads = buildPayloads();
 
-                DatagramPacket packet = new DatagramPacket(payload, payload.length, address, 6666);
-                socket.send(packet);
+                if (DEBUG) {
+                    Log.d("REQUEST", "Payloads count: " + payloads.size());
+                    Log.d("REQUEST", "Payloads length: ");
+                    for (int i = 0; i < payloads.size(); i++) {
+                        Log.d("REQUEST", i + ":" + payloads.get(i).length);
+                    }
+                }
+
+                for (int i = 0; i < payloads.size(); i++) {
+                    byte[] payload = payloads.get(i);
+                    DatagramPacket packet = new DatagramPacket(payload, payload.length, address, 5000);
+                    socket.send(packet);
+                }
 
                 lastScreenshotVersion = screenshotVersion;
             }
         }
 
-        private String buildPayload() {
+        private List<byte[]> buildPayloads() {
             JSONObject jsonObject = new JSONObject();
             try {
                 jsonObject.put("sessionId", ScreenIntercepter.SESSION_ID);
@@ -96,7 +115,31 @@ public class UdpSocketSender extends AbstractSender {
                 e.printStackTrace();
             }
 
-            return jsonObject.toString();
+            byte[] data = jsonObject.toString().getBytes();
+            List<byte[]> payloads = new ArrayList<byte[]>();
+            byte payloadsCount = (byte) (data.length / MAX_SIZE + 1);
+            for (byte i = 0; i < payloadsCount; i++) {
+                int from;
+                int to;
+                int length;
+                if (i < payloadsCount - 1) {
+                    from = i * MAX_SIZE;
+                    to = i * MAX_SIZE + MAX_SIZE;
+                } else {
+                    from = i * MAX_SIZE;
+                    to = data.length;
+                }
+                length = to - from;
+                if (DEBUG) {
+                    Log.d("REQUEST", "from: " + from + " to: " + to + " length: " + length);
+                }
+                byte[] payload = new byte[length + 2];
+                System.arraycopy(data, from, payload, 2, length);
+                payload[0] = (byte) (i + 1);
+                payload[1] = payloadsCount;
+                payloads.add(payload);
+            }
+            return payloads;
         }
 
         private void load() {
