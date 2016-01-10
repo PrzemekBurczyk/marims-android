94e03cf9c4fa04d5405684f7ef0fe18bcd80328c
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-01-10 10:44:08 +0100

    Added hub module with basic views and master/detail flow.

---
+++ hub/src/main/java/pl/edu/agh/marims/hub/FileDetailFragment.java
@@ -0,0 +1,69 @@
+package pl.edu.agh.marims.hub;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import pl.edu.agh.marims.hub.dummy.DummyContent;
+
+/**
+ * A fragment representing a single File detail screen.
+ * This fragment is either contained in a {@link FileListActivity}
+ * in two-pane mode (on tablets) or a {@link FileDetailActivity}
+ * on handsets.
+ */
+public class FileDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public FileDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.content);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.file_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.file_detail)).setText(mItem.details);
+        }
+
+        return rootView;
+    }
+}
