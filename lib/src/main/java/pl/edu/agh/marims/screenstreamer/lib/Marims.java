d083a2da616208c2232f97b32c4e7e4afbe6f383
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-01-26 16:24:19 +0100

    Implemented logs intercepter with sending through websocket connection.

--- lib/src/main/java/pl/edu/agh/marims/screenstreamer/lib/Marims.java
+++ lib/src/main/java/pl/edu/agh/marims/screenstreamer/lib/Marims.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import pl.edu.agh.marims.screenstreamer.lib.intent.IntentReader;
 import pl.edu.agh.marims.screenstreamer.lib.network.sender.SenderType;
 import pl.edu.agh.marims.screenstreamer.lib.screen.intercepter.Intercepter;
+import pl.edu.agh.marims.screenstreamer.lib.screen.intercepter.LogsIntercepter;
 import pl.edu.agh.marims.screenstreamer.lib.screen.intercepter.ScreenIntercepter;
 import pl.edu.agh.marims.screenstreamer.lib.screen.intercepter.StatisticsCallback;
 import pl.edu.agh.marims.screenstreamer.lib.screen.manipulator.ScreenManipulator;
@@ -16,6 +17,7 @@ public class Marims {
 
     private static final String SESSION_ID_KEY = "sessionId";
     private final Intercepter screenIntercepter;
+    private final Intercepter logsIntercepter;
     private final ScreenManipulator screenManipulator;
 
     public Marims(Activity activity, View view, String serverUrl) {
@@ -27,6 +29,7 @@ public class Marims {
         }
 
         screenIntercepter = new ScreenIntercepter(activity, view, serverUrl, sessionId);
+        logsIntercepter = new LogsIntercepter(activity, serverUrl, sessionId);
         screenManipulator = new ScreenManipulator(activity, view, serverUrl, sessionId);
     }
 
@@ -45,11 +48,14 @@ public class Marims {
     public void onResume() {
         screenIntercepter.initialize();
         screenIntercepter.start();
+        logsIntercepter.initialize();
+        logsIntercepter.start();
         screenManipulator.initialize();
     }
 
     public void onPause() {
         screenIntercepter.stop();
+        logsIntercepter.stop();
         screenManipulator.stop();
     }
 }
---
+++ lib/src/main/java/pl/edu/agh/marims/screenstreamer/lib/network/sender/WebsocketSender.java
@@ -0,0 +1,5 @@
+package pl.edu.agh.marims.screenstreamer.lib.network.sender;
+
+public class WebsocketSender<T> extends AbstractSender<T> {
+
+}
