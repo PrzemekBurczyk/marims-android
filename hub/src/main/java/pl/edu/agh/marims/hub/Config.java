04548cabbdbd7bcb101e0d5dcf14e9a9c1984584
Przemek Burczyk <przemek.burczyk@gmail.com>
2016-02-23 19:53:33 +0100

    Incremented versions. Included production server url.

--- hub/src/main/java/pl/edu/agh/marims/hub/Config.java
+++ hub/src/main/java/pl/edu/agh/marims/hub/Config.java
@@ -1,6 +1,6 @@
 package pl.edu.agh.marims.hub;
 
 public class Config {
-    public static String SERVER_URL = "http://192.168.0.16:5000/";
+    public static String SERVER_URL = "http://marims.pl/";
     public static String SOCKET_IO_ENDPOINT = "client";
 }
